1ccb48ee33ea06e3d0324188b24fbb390d7ccc45
zellski1234 <zellski1234@gmail.com>
2022-08-30 12:57:47 +0100

    first  commit

---
+++ src/server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000
+
+
+// app.use(express.static("public"))
+
+app.get("/", (req, res)=> {
+	res.send("Hi my name is Dan")
+
+});
+
+app.get("/hello", (req, res)=> {
+	console.log(req.query.name)
+
+	if (req.query.name){
+		res.send({ name: req.query.name});
+	}
+	else {
+		res.send({error: "no name found"})
+	}
+});
+
+app.get("/myinfo", (req, res)=> {
+	res.send({
+		name: "Hamza",
+		age: "25"});
+});
+// http://localhost:5000/hello?name=dan&age=35&somethingelse
+
+app.get("/users/:id", (req, res)=> {
+	console.log(req.params.id)
+
+	res.send({id: req.params.id})
+});
+
+
+app.listen(port, () => {
+	console.log(`listening on port ${port}`)
+})
