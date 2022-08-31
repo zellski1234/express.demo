0f7b3dfdf619a05e27639860485e7ef8709f474a
zellski1234 <zellski1234@gmail.com>
2022-08-31 11:24:52 +0100

    updated

--- src/movies/movieControllers.js
+++ src/movies/movieControllers.js
@@ -1,12 +1,31 @@
 const Movies = require("../db/movieModel");
 
 
-exports.listMovies = (req, res) => {
-    let list = await Movies.find({})
-    res.send(list)
+exports.listMovies = async (req, res) => {
+    try {
+        let list = await Movies.find({})
+        res.send(list)
+    } catch {
+
+
+    }
 }
 
 exports.addMovie = async (req, res) => {
-    await Movies.create({title: req.body.title, actor: req.body.actor});
-    res.status(201).send({title: req.body.title, actor: req.body.actor})
+    try {
+        if (req.body.title && req.body.actor){
+            console.log(req.body)
+            await Movies.create({title: req.body.title, actor: req.body.actor});
+            res.status(201).send({title: req.body.title, actor: req.body.actor});
+        }
+        else {
+            console.log("no title or actor found")
+            res.status(400).send({error: "no title or actor found"})
+        }
+    } catch (e) {
+        console.log("error in add movie")
+        res.status(500).send({error:"internal server error"})
+        console.log(e)
+
+    }
 }
